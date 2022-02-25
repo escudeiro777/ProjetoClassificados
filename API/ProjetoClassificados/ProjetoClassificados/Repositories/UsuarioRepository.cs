e6315b7a286f8eaa6889a9cce893abc870fa9671
MurilloAssis <murillo98asiss@gmail.com>
2022-02-25 15:02:31 -0300

    Login e Criptografia de Senha adicionado

--- API/ProjetoClassificados/ProjetoClassificados/Repositories/UsuarioRepository.cs
+++ API/ProjetoClassificados/ProjetoClassificados/Repositories/UsuarioRepository.cs
@@ -1,18 +1,42 @@
-﻿using ProjetoClassificados.Domains;
+﻿using ProjetoClassificados.Contexts;
+using ProjetoClassificados.Domains;
 using ProjetoClassificados.Interfaces;
+using ProjetoClassificados.Utils;
+using System;
+using System.Linq;
 
 namespace ProjetoClassificados.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
+        ECS_Context ctx = new();
         public void CadastrarUsuario(Usuario novoUsuario)
         {
-            throw new System.NotImplementedException();
+            ctx.Usuarios.Add(novoUsuario);
+            ctx.SaveChanges();
         }
 
         public Usuario Login(string email, string senha)
         {
-            throw new System.NotImplementedException();
+            Usuario usuario = ctx.Usuarios.FirstOrDefault(u => u.Email == email);
+
+            if (usuario != null)
+            {
+                if (usuario.Senha.Length != 60 && usuario.Senha[0].ToString() != "$")
+                {
+                    string senhaHash = Criptografia.gerarHash(senha);
+                    usuario.Senha = senhaHash;
+                    ctx.Usuarios.Update(usuario);
+                    ctx.SaveChanges();
+                    return usuario;
+                }
+                bool confere = Criptografia.compararSenha(senha, usuario.Senha);
+                if (confere)
+                {
+                    return usuario;
+                }
+            }
+            return null;
         }
     }
 }
