b104bf3cdba7b43237ecc9ab5cf90da028312417
MurilloAssis <murillo98asiss@gmail.com>
2022-02-25 13:11:02 -0300

    Repositorio e Interface de usuario criadas

---
+++ API/ProjetoClassificados/ProjetoClassificados/Repositories/UsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProjetoClassificados.Domains;
+using ProjetoClassificados.Interfaces;
+
+namespace ProjetoClassificados.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public void CadastrarUsuario(Usuario novoUsuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Usuario Login(string email, string senha)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
