dd417db0fdf23f93a305f1db16b72c48b873a4c7
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-02-25 16:10:46 -0300

    interface pronta e começando Repositories

---
+++ API/ProjetoClassificados/ProjetoClassificados/Interfaces/ITipoUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoClassificados.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoClassificados.Interfaces
+{
+    interface ITipoUsuarioRepository
+    {
+        void Cadastrar(TipoUsuario cadastrarTipoUsario);
+    }
+}
---
+++ API/ProjetoClassificados/ProjetoClassificados/Repositories/SituacaoRepository.cs
@@ -0,0 +1,40 @@
+﻿using ProjetoClassificados.Contexts;
+using ProjetoClassificados.Domains;
+using ProjetoClassificados.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoClassificados.Repositories
+{
+    public class SituacaoRepository : ISituacaoRepository
+    {
+        ECS_Context ctx = new ECS_Context();
+
+        public Situacao BuscarPorId(int id)
+        {
+            return ctx.Situacaos.FirstOrDefault(x => x.IdSituacao == id);
+        }
+
+        public void Cadastrar(Situacao novaSituacao)
+        {
+            ctx.Situacaos.Add(novaSituacao);
+
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            ctx.Situacaos.Remove(BuscarPorId(id));
+
+            ctx.SaveChanges();
+        }
+
+        public List<Situacao> Listar()
+        {
+            throw new NotImplementedException();
+            //Arrumando banco de Dados..... Ainda não é possivel fazer este 
+        }
+    }
+}
