aa9184f07c309d1ae02ab48d0bab67b576c8255e
Fábio Henrique Quatrochi <39220866838@info.scs>
2022-03-03 16:07:46 -0300

    controllers...

---
+++ API/ProjetoClassificados/ProjetoClassificados/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoClassificados.Domains;
+using ProjetoClassificados.Interfaces;
+using ProjetoClassificados.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoClassificados.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    //http://localhost:5000/api/tiposUsuarios
+
+    [ApiController]
+    public class TipoUsuarioController : ControllerBase
+    {
+        private ITipoUsuarioRepository _tipousuarioRepository { get; set; }
+
+        public TipoUsuarioController()
+        {
+            _tipousuarioRepository = new TipoUsuarioRepository();
+        }
+
+        [Authorize(Roles = "1")]
+        [HttpPost]
+        public IActionResult Post(TipoUsuario novoTipo)
+        {
+            _tipousuarioRepository.Cadastrar(novoTipo);
+
+            return StatusCode(201);
+        }
+
+    }
+}
